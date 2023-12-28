12e128a251c02ae4ceb882dff920e7546930a397
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-12-28 17:37:03 +0500

    add background Image

--- app/components/ourwork/page.tsx
+++ app/components/ourwork/page.tsx
@@ -1,5 +1,21 @@
 import React from "react";
-
+import Image from "next/image";
+import bg_myImg from "./backgroundImage/bg_image.png";
 export default function OurWork() {
-  return <div className=" absolute top-[1400px]">hi this is develoepr</div>;
+  return (
+    <div className="absolute top-[1300px]">
+      <Image
+        alt="Mountains"
+        src={bg_myImg}
+        placeholder="blur"
+        quality={100}
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+          width: "100%",
+          height: "50%",
+        }}
+      />
+    </div>
+  );
 }
