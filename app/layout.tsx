f188bebf8efeb174911fe041cffaa832263fbdf5
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-12-28 17:48:11 +0500

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={poppins.variable}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
